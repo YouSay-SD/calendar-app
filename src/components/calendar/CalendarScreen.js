3c5d36dc9b647a5b8752306d04cad77f985fb01d
YouSay-SD <yousay.sd@gmail.com>
2020-09-26 20:28:05 -0300

    Added Moment And BigCalendar Npm

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { Navbar } from '../ui/Navbar';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+
+const localizer = momentLocalizer( moment );
+
+const events = [{
+  title: 'Cumpleanos del Jefe',
+  start: moment().toDate(),
+  end: moment().add( 2, 'hours' ).toDate(),
+  bgColor: '#fafafa'
+}];
 
 export const CalendarScreen = () => {
   return (
     <div>
 
       <Navbar />
+
+      <Calendar 
+        localizer={ localizer }
+        events={ events }
+        startAccessor="start"
+        endAccessor="end"
+      />
       
     </div>
   )
