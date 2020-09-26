e4b955b0d8be12fea2e0e0e1ce7c7f949688f2a5
YouSay-SD <yousay.sd@gmail.com>
2020-09-26 20:40:46 -0300

    Added Styles For Calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navbar } from '../ui/Navbar';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const localizer = momentLocalizer( moment );
 
@@ -14,7 +15,7 @@ const events = [{
 
 export const CalendarScreen = () => {
   return (
-    <div>
+    <div className="calendar-screen">
 
       <Navbar />
 
