d1f873caa622d471a889dc454dc1227721932351
YouSay-SD <yousay.sd@gmail.com>
2020-09-27 01:46:46 -0300

    Create CalendarEvents

---
+++ src/components/calendar/CalendarEvent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const CalendarEvent = ({ event }) => {
+ 
+  const { title, user } = event;
+
+  return (
+    <div>
+    
+      <strong>{ title }</strong>
+
+      <span> - { user.name }</span>
+
+    </div>
+  )
+}
